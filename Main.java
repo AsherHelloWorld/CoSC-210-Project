8f2c5673e12068b84d762f8ee296692f6edd621b
AsherHelloWorld <9jqxjk2m7r@privaterelay.appleid.com>
2026-01-22 14:53:29 -0800

    Created simple task class, and a main to test stuff.

---
+++ Main.java
@@ -0,0 +1,9 @@
+public main{
+    public static void main(String[] args){
+        Task test = new Test("Charlie", 10, 15);
+
+        test.getName();
+        test.getDate();
+        test.getTime();
+    }
+}
---
+++ Task.java
@@ -0,0 +1,36 @@
+public class Task{
+    String name;
+    int date;
+    int time;
+
+    public Task(String name, int date, int time){
+        name = name;
+        date = date;
+        time = time;
+    }
+
+    private getName(){
+        return this.name;
+    }
+    private void setName(String name){
+        this.name = name;
+    }
+    private getDate(){
+        return this.date;
+    }
+    private void setDate(int date){
+        this.date = date;
+    }
+    private getTime(){
+        return this.time;
+    }
+    private void setTime(int time){
+        this.time = time;
+    }
+
+
+
+
+
+
+}
