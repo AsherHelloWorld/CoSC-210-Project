09f1cd068df0388f0373e020653c5bdad0d40e7c
Asher Nathan <9jqxjk2m7r@privaterelay.appleid.com>
2026-01-29 18:09:44 -0800

    Didn't save the planner method gotta push again

---
+++ src/main/java/Planner.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Planner {
+    ArrayList<Task> tasks = new ArrayList<Task>();
+
+    public void addTask(){
+        Scanner s = new Scanner(System.in);
+        System.out.printline("Enter task name:");
+        
+            
+            
+            
+            
+            
+        tasks.add(task);
+    }
+
+}
