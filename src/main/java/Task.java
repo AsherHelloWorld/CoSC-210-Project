fe4bfd2a224f70d73585069c674c228d8a95b298
Quinn Handy <quinnhandy9@gmail.com>
2026-01-29 17:42:34 -0800

    Added Requires/Modifies/Effects Stubs, and no arg constructor

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,6 +4,7 @@ public class Task{
     private int time;
     private String description;
     private String location;
+
     public Task(String name, String date, int time, String description, String location){
 
         this.name = name;
@@ -14,33 +15,70 @@ public class Task{
         
     }
 
+    public Task() {
+        this.name = "Default Task";
+        this.date = "Monday";
+        this.time = 1;
+        this.description = "No Description";
+        this.location = "No Location";
+    }
+
+    // EFFECTS: returns the name of this task.
     public String getName(){
         return this.name;
     }
+
+    // MODIFIES: this
+    // EFFECTS: sets the name of this task to the given name.
+    // REQUIRES: name is not null
     public void setName(String name){
         this.name = name;
     }
+
+    // EFFECTS: returns the date of this task.
     public String getDate(){
         return this.date;
     }
+
+    // REQUIRES: date is not null
+    // MODIFIES: this
+    // EFFECTS: sets the date of this task to the given date.
     public void setDate(String date){
         this.date = date;
     }
+
+    // EFFECTS: returns the time of this task.
     public int getTime(){
         return this.time;
     }
+
+    // REQUIRES: time is a positive integer (in hours)
+    // MODIFIES: this
+    // EFFECTS: sets the time of this task to the given time.
     public void setTime(int time){
         this.time = time;
     }
+
+    // EFFECTS: returns the description of this task.
     public String getDescription(){
         return this.description;
     }
+
+    // REQUIRES: description is not null
+    // MODIFIES: this
+    // EFFECTS: sets the description of this task to the given description.
     public void setDescription(String description){
         this.description = description;
     }
+
+    // EFFECTS: returns the location of this task.
     public String getLocation(){
         return this.location;
     }
+
+    // REQUIRES: location is not null
+    // MODIFIES: this
+    // EFFECTS: sets the location of this task to the given location.
     public void setLocation(String location){
         this.location = location;
     }
